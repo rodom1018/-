081aa1bf25269915351d4d2d9a311c74ef2297dd
rodom_macbook <enjoying1018@naver.com>
2022-09-20 09:05:56 +0900

    kakao 2022-1번

---
+++ 카카오/2022recuitment/1번_k진수에서소수개수구하기.cpp
@@ -0,0 +1,57 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+
+string convert(long n, long k){
+    string temp_result = "";
+    while(n>k){
+        temp_result = to_string(n%k) + temp_result ;
+        n /= k;
+        // cout<<temp_result << "// " << n << endl;
+    }
+    temp_result = to_string(n) + temp_result;
+    //cout << temp_result;
+    return temp_result;
+}
+
+bool isPrime(long n) {
+    if (n < 2) return false;
+    for (long i = 2; i * i <= n; i++) {
+        if (n % i == 0) return false;
+    }
+    return true;
+}
+
+int solution(int n, int k) {
+    string con_string = convert(n, k );
+    //cout<<con_string << endl; 잘되는거확인.
+    long answer = 0;
+    long start = 0;
+    long len = 0;
+    string cmp = "0";
+    while(start < con_string.length()){
+        while(con_string.substr(start+len , 1).compare(cmp) != 0){
+            len += 1;
+            if(start+len >= con_string.length()) break;
+        }
+
+        if(len == 0 ){
+            start += 1;
+            if(start >= con_string.length()) break;
+            continue;
+        }
+        long now_num = stol(con_string.substr(start, len));
+        //cout<< start << "//" << len << "//" << now_num << endl;
+        if(isPrime(now_num)) answer ++ ;
+
+        start += len;
+
+        len = 0 ;
+
+    }
+    return answer;
+
+}
